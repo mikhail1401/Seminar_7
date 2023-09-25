184c978e5db053050187f2f189e5b3c377fca870
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2023-09-25 01:24:32 -0400

    Completed task 49 in my own way

--- Program.cs
+++ Program.cs
@@ -48,3 +48,51 @@ for (int row = 0; row < table.GetLength(0); row++)
     }
     Console.WriteLine();
 }
+
+
+System.Console.WriteLine("\nTask 49");
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// А потом так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+System.Console.Write("Enter the number of rows: ");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter the number of columns: ");
+int columns1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix1 = new int[rows1, columns1];
+
+Console.WriteLine("Original matrix1:");
+for (int row = 0; row < matrix1.GetLength(0); row++)
+{
+    for (int column = 0; column < matrix1.GetLength(1); column++)
+    {
+        matrix1[row, column] = new Random().Next(0, 10);
+        Console.Write(matrix1[row, column] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Modified matrix1:");
+for (int row = 0; row < matrix1.GetLength(0); row++)
+{
+    for (int column = 0; column < matrix1.GetLength(1); column++)
+    {
+        if (row>0 && column>0)
+        {
+            if (row%2==0 && column%2==0)
+            {
+                matrix1[row, column] = matrix1[row, column] * matrix1[row, column];
+            }
+        }
+        Console.Write(matrix1[row, column] + " ");
+    }
+    Console.WriteLine();
+}
