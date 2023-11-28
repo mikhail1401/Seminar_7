284c28835ba07d18b5e83941bf311df95636ec60
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2023-11-28 00:13:08 -0500

    Completed task 51

--- obj/Debug/net6.0/Seminar_7.AssemblyInfo.cs
+++ obj/Debug/net6.0/Seminar_7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar_7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738aa97034f89ec4126a18aa04c2164fe8e674c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar_7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar_7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
